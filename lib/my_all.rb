53cb6f6f16ad60f75fedbb77b14f6b1c275d2ae4
Sierra Wallace <sskwallace@gmail.com>
2020-08-10 22:46:46 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 def my_all?(collection)
-i = 0 
-while i < collection.size 
-yield(collection[i])
-i = i + 1 
+  i = 0 
+  while i < collection.size 
+  yield(collection[i])
+  i = i + 1 
+end
 end
